4e31e55d020d8cc8831d5122352471a3ced94d17
Valeriy <mr.robariii@gmail.com>
2021-01-30 15:23:15 +0200

    Создал скрипт DraggableCard
    Скрипт при помощи, которого перемещается карточка из слота в слот

---
+++ Rescue Squad/Assets/Scripts/Mechanics/DraggableCard.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.Mechanics
+{
+
+    public class DraggableCard : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+
+        [SerializeField]
+        private Canvas _canvas;
+
+        [SerializeField]
+        private RectTransform _rectTransform;
+
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _canvasGroup.blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _canvasGroup.blocksRaycasts = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
