d0954c9754bb420d44711e75faf82cb9c9ab7172
Nikita <Nkanunov@gmail.com>
2021-01-30 18:10:44 +0200

    Auto stash before rebase of "origin/main"

---
+++ Rescue Squad/Assets/Scripts/Managers/TaskManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections.Generic;
+
+namespace Game.Managers
+{
+    public class TaskManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private List<ScriptableObject> _tasks;
+
+        [SerializeField]
+        private GameObject _marker;
+
+        private float timeLeft;
+
+        private void Awake()
+        {
+        }
+
+        private void Update()
+        {
+            SpawnTaskMarkerOnMap();
+        }
+        private ScriptableObject GetRandomTask()
+        {
+            var ramdomTaskItdex = Random.Range(0, _tasks.Count - 1);
+            var task = _tasks[ramdomTaskItdex];
+            return task;
+        }
+
+        private void SpawnTaskMarkerOnMap()
+        {
+            var spawn = new Vector2(50, 50);
+            var enemyCar = Instantiate(_marker, spawn, Quaternion.identity);
+        }
+    }
+}
