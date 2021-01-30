2d71e2509541b2cc5b90af02d6c117279d35ea64
Valeriy <mr.robariii@gmail.com>
2021-01-30 12:09:49 +0200

    Создал папку с разной фигнёй для тестов(Потом будет удалена)
    Добавил скрипт для проверки отработки ивента

---
+++ Rescue Squad/Assets/Scripts/TestShit/TriggerEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game.Testing
+{
+
+    public class TriggerEvent : MonoBehaviour
+    {
+        [SerializeField]
+        private EventListener _updateListener;
+
+        [SerializeField]
+        private EventDispatcher _pointOfInterestDispatcher;
+
+        private void OnEnable()
+        {
+            _updateListener.OnEventHappened += TriggerEventMethod;
+        }
+
+        private void OnDisable()
+        {
+            _updateListener.OnEventHappened -= TriggerEventMethod;
+        }
+
+        private void TriggerEventMethod()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _pointOfInterestDispatcher.Dispatch();
+                Debug.Log("Dispatching");
+            }
+        } 
+    }
+}
+
