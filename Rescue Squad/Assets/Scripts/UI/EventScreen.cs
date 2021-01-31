21298852c94f2d4c9052f13c6fafa8766171a21e
Valeriy <mr.robariii@gmail.com>
2021-01-31 09:48:54 +0200

    EventScreen script created
    to show info in eventscreen

---
+++ Rescue Squad/Assets/Scripts/UI/EventScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Game.UI
+{
+
+    public class EventScreen : MonoBehaviour
+    {
+
+
+        public void ClosePanel()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void SendBoyz()
+        {
+            Debug.Log("Boyz are sent");
+            gameObject.SetActive(false);
+        }
+    }
+}
