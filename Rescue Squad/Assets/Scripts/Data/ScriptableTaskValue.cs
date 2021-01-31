fa41ffb900895615fdeb9ca382d36d2469480124
Valeriy <mr.robariii@gmail.com>
2021-01-31 09:08:12 +0200

    ScriptableTaskValue script created
    For storing task

---
+++ Rescue Squad/Assets/Scripts/Data/ScriptableTaskValue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Data
+{
+
+    [CreateAssetMenu(fileName = "New Task", menuName = "GameData/Task")]
+    public class ScriptableTaskValue : ScriptableObject
+    {
+
+        public RescueEventSO value;
+    }
+}
