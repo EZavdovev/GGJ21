b4096c3a433958ba8dea6f959786e3ade687b190
Valeriy <mr.robariii@gmail.com>
2021-01-31 09:38:03 +0200

    ScriptableGameObject script created
     to store GO

---
+++ Rescue Squad/Assets/Scripts/Data/ScriptableGameObjectValue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Data
+{
+
+    [CreateAssetMenu(fileName = "New GameObjectValue", menuName = "GameData/GO")]
+    public class ScriptableGameObjectValue : ScriptableObject
+    {
+
+        public GameObject value;
+    }
+}
