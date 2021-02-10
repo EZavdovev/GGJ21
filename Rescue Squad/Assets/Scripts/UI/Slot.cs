e7c1fa08015cdea51fa1b5f253a22e8dbbf1971f
Valeriy <mr.robariii@gmail.com>
2021-02-10 06:11:44 +0200

    Slot script created
    Sets a card to a proper place

---
+++ Rescue Squad/Assets/Scripts/UI/Slot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Game.Data;
+
+
+namespace Game.UI
+{
+
+    public class Slot : MonoBehaviour, IDropHandler
+    {
+
+        [SerializeField]
+        private ScriptableGameObjectValue _itemBeingDragged;
+
+        public GameObject Item
+        {
+            get
+            {
+                if (transform.childCount > 0)
+                {
+                    return transform.GetChild(0).gameObject;
+                }
+                return null;
+            }
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (!Item)
+            {
+                _itemBeingDragged.value.transform.SetParent(transform);
+            }
+        }
+    }
+}
+
