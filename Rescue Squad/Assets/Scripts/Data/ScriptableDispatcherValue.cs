99dcb68c030d499bc01f4ad10edf5400fc837eee
Valeriy <mr.robariii@gmail.com>
2021-02-10 09:10:08 +0200

    ScriptableDispatcherValue script created
    Contains dispatcher

---
+++ Rescue Squad/Assets/Scripts/Data/ScriptableDispatcherValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Events;
+
+
+namespace Game.Data
+{
+
+    [CreateAssetMenu(menuName = "GameData/EventValue", fileName = ("New Event Value"))]
+    public class ScriptableDispatcherValue : ScriptableObject
+    {
+
+        public EventDispatcher value;
+    }
+}
