0c10781e688031ef89076dfcea0ba0ffa395d879
Valeriy <mr.robariii@gmail.com>
2021-03-02 19:43:38 +0200

    ScriptableEventListener asset created

---
+++ Rescue Squad/Assets/Scripts/Data/ScriptableEventListenerValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Events;
+
+
+namespace Game.Data
+{
+
+    [CreateAssetMenu(menuName = "GameData/Event Listener Value", fileName = ("New Event Listener Value"))]
+    public class ScriptableEventListenerValue : ScriptableObject
+    {
+
+        public EventListener value;
+    }
+}
