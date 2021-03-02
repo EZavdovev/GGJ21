c8965a4a8b85c61b3f54cabeb7f9eb05c6693b65
Valeriy <mr.robariii@gmail.com>
2021-03-02 19:44:10 +0200

    ScriptableDispatcherValue script updated
    Chaged a name for creation

--- Rescue Squad/Assets/Scripts/Data/ScriptableDispatcherValue.cs
+++ Rescue Squad/Assets/Scripts/Data/ScriptableDispatcherValue.cs
@@ -5,7 +5,7 @@ using Events;
 namespace Game.Data
 {
 
-    [CreateAssetMenu(menuName = "GameData/EventValue", fileName = ("New Event Value"))]
+    [CreateAssetMenu(menuName = "GameData/Event Dispatcher Value", fileName = ("New Event Value"))]
     public class ScriptableDispatcherValue : ScriptableObject
     {
 
