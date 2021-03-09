21a7d82346f3a9d98d0781dcf2bce5a8035afb1a
Valeriy <mr.robariii@gmail.com>
2021-03-09 18:26:40 +0200

    CardSorter script created
    Updating visual and logic of cards in stacks

---
+++ Rescue Squad/Assets/Scripts/Managers/CardSorter.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using Events;
+using Game.UI;
+
+
+namespace Game.Managers
+{
+
+    public class CardSorter : MonoBehaviour
+    {
+
+        [SerializeField]
+        private EventListener _cardSetEventListener;
+
+        [SerializeField]
+        private Transform _cardsStack;
+        
+        [SerializeField]
+        private Transform _cardsTaskStack;
+
+        private void OnEnable()
+        {
+            _cardSetEventListener.OnEventHappened += UpdateCardsStack;
+            _cardSetEventListener.OnEventHappened += UpdateTaskStack;
+        }
+
+        private void OnDisable()
+        {
+            _cardSetEventListener.OnEventHappened -= UpdateCardsStack;
+            _cardSetEventListener.OnEventHappened -= UpdateTaskStack;
+        }
+
+        private void UpdateCardsStack()
+        {
+            for (int i = 0; i < _cardsStack.childCount; i++)
+            {
+                _cardsStack.GetChild(i).TryGetComponent<Slot>(out var slot);
+                if (slot.Item != null)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        _cardsStack.GetChild(j).TryGetComponent<Slot>(out var emptySlot);
+                        if (emptySlot.Item == null)
+                        {
+                            emptySlot.Item = slot.Item;
+                            if(slot.transform.childCount > 0)
+                            {
+                                slot.transform.GetChild(0).SetParent(emptySlot.transform);
+                            }
+                        }
+                    }
+                }
+                slot.CheckItem();
+            }
+        }
+
+        private void UpdateTaskStack()
+        {
+            for (int i = 0; i < _cardsTaskStack.childCount; i++)
+            {
+                _cardsTaskStack.GetChild(i).TryGetComponent<Slot>(out var slot);
+                if (slot.Item != null)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        _cardsTaskStack.GetChild(j).TryGetComponent<Slot>(out var emptySlot);
+                        if (emptySlot.Item == null)
+                        {
+                            emptySlot.Item = slot.Item;
+                            if (slot.transform.childCount > 0)
+                            {
+                                slot.transform.GetChild(0).SetParent(emptySlot.transform);
+                            }
+                        }
+                    }
+                }
+                slot.CheckItem();
+            }
+        }
+    }
+}
+
