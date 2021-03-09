3282eda6381a2ac7d6ad5bf33bfd43782c78a4d4
Valeriy <mr.robariii@gmail.com>
2021-03-09 18:20:23 +0200

    UIManager script created
    Keeps logic of all screens in the game

---
+++ Rescue Squad/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Game.UI
+{
+
+    public class UIManager : MonoBehaviour
+    {
+
+        public static UIManager instance;
+
+        [SerializeField]
+        private GameObject _gameScreen;
+
+        [SerializeField]
+        private GameObject _eventScreen;
+
+        [SerializeField]
+        private GameObject _menuScreen;
+
+        [SerializeField]
+        private GameObject _settingsScreen;
+
+
+
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void EnableGameScreens()
+        {
+            _gameScreen.SetActive(true);
+            _eventScreen.SetActive(false);
+            _menuScreen.SetActive(false);
+            _settingsScreen.SetActive(false);
+        }
+    }
+}
+
